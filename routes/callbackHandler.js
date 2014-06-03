e8d3e6f7a50751cdf81b62d70e993bd672a0a47e
Himanshu Sharma <himanshu.sharma@makepositive.com>
2014-06-03 16:19:36 +0530

    f

--- routes/callbackHandler.js
+++ routes/callbackHandler.js
@@ -30,17 +30,13 @@ function handleFlashingLight(value) {
 
 exports.allGood = function() {
 
-	if(interval == undefined) interval = 500;
-	//set default time for blinkTime
-	//number of times to blink the light on and off
-	if(blinkTime == undefined) blinkTime = 5;
-
-	var request = new Object();
-  	request.body = {"pin":11};
-
   	var flag = false;
 
-  	var intrvl = setInterval(function(request){
+  	var intrvl = setInterval(function(){
+  		
+  		var request = new Object();
+  		request.body = {"pin":11};
+
   		if(!flag){
 	  		fnRouter.turnON(request,function(results){
 				// console.log('Good Pin ON');
